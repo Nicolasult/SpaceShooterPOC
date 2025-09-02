71c99f722a27857a8e910ecd224cef5e85fe571a
nicolasult <nicolas.ultrillo@gmail.com>
2025-09-02 22:26:48 +0200

    add level info script and resources

---
+++ content/ui/level_info.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name LevelInfo
+
+@export var title: String = "Level"
+@export var scene: PackedScene
+@export var thumbnail: Texture2D
+@export_multiline var description: String = ""
+@export var unlocked: bool = true
